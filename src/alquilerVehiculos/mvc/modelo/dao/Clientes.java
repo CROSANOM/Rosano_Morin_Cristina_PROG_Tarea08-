20f54646dc23e6f06307051cc70f22aa470ee75c
crosanom <crosanom@192.168.1.39>
2018-03-03 11:38:11 +0100

    Copiar el proyecto corregido de la tarea7 para iniciar la tarea 8

---
+++ src/alquilerVehiculos/mvc/modelo/dao/Clientes.java
@@ -0,0 +1,224 @@
+package alquilerVehiculos.mvc.modelo.dao;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOError;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.Arrays;
+
+import alquilerVehiculos.mvc.modelo.dominio.Cliente;
+import alquilerVehiculos.mvc.modelo.dominio.ExcepcionAlquilerVehiculos;
+
+public class Clientes {
+
+	private Cliente[] clientes;
+	private final int MAX_CLIENTES = 5;
+
+	// atributo ruta del fichero
+
+	private final String FICHERO_CLIENTES = "datos/clientes.dat";
+
+	// constructor
+
+	public Clientes() {
+		clientes = new Cliente[MAX_CLIENTES];
+	}
+
+	// getClientes ( se usa return de obtenerClientes )
+
+	/**
+	 * @return Clientes( copias )
+	 */
+	public Cliente[] getClientes() {
+		return clientes.clone();
+	}
+
+	// Metodos de lectura - escritura de fichero
+
+	public void leerClientes() {
+
+		// 1º crear fichero usando la constante ruta
+
+		File fichero = new File(FICHERO_CLIENTES);
+
+		// 2º crear Objeto de la clase ObjectInputStrean
+
+		ObjectInputStream entrada;
+
+		try {
+
+			entrada = new ObjectInputStream(new FileInputStream(fichero));
+
+			try {
+				clientes = (Cliente[]) entrada.readObject();
+				entrada.close();
+				System.out.println("fichero leido correctamente");
+				Cliente.aumentarUltimoIdentificador(calcularUltimoIdentificador());
+
+			} catch (ClassNotFoundException e) {
+
+				System.out.println("No puedo encontrar la clase ");
+
+			} catch (IOException e) {
+
+				System.out.println("Error inesperado de Entrada/Salida");
+			}
+
+		} catch (IOException e) {
+
+			System.out.println("No puede abrir el fichero de clientes");
+
+		}
+
+	}
+
+	private int calcularUltimoIdentificador() {
+		int ultimoIdentificador = 0;
+		int i = 0;
+		while (clientes[i] != null) {
+			if (clientes[i].getIdentificador() > ultimoIdentificador)
+				ultimoIdentificador = clientes[i].getIdentificador();
+			i++;
+		}
+		return ultimoIdentificador;
+	}
+
+	// escribirClientes
+
+	public void escribirClientes() {
+		File fichero = new File(FICHERO_CLIENTES);
+		try {
+			ObjectOutputStream salida = new ObjectOutputStream(new FileOutputStream(fichero));
+			salida.writeObject((Cliente[]) clientes);
+			salida.close();
+			System.out.println("Fichero clientes escrito satisfactoriamente.");
+		} catch (FileNotFoundException e) {
+			System.out.println("No puedo crear el fichero de clientes");
+		} catch (IOException e) {
+			System.out.println("Error inesperado de Entrada/Salida");
+		}
+	}
+
+	/*
+	 * anadir Clientes (BuscarIndiceLibreComprobandoExistencia,
+	 * indiceNoSuperaTamano)
+	 */
+
+	/**
+	 * @param cliente
+	 */
+
+	public void anadirCliente(Cliente cliente) {
+		int indice = buscarPrimerIndiceLibreComprobandoExistencia(cliente);
+		if (indiceNoSuperaTamano(indice))
+			clientes[indice] = new Cliente(cliente);
+		else
+			throw new ExcepcionAlquilerVehiculos("El array de clientes esta lleno.");
+	}
+
+	// metodo buscarPrimerIndiceLibreComprobandoExistencia
+	/**
+	 * @param cliente
+	 * @return indice
+	 */
+	private int buscarPrimerIndiceLibreComprobandoExistencia(Cliente cliente) {
+		int indice = 0;
+		boolean clienteEncontrado = false;
+		while (indiceNoSuperaTamano(indice) && !clienteEncontrado) {
+			if (clientes[indice] == null)
+				clienteEncontrado = true;
+			else if (clientes[indice].getDni().equals(cliente.getDni()))
+				throw new ExcepcionAlquilerVehiculos("Ya existe un cliente con ese DNI");
+			else
+				indice++;
+		}
+		return indice;
+	}
+
+	// metodo indiceNoSuperaTamano
+
+	/**
+	 * @param indice
+	 * @return boolean
+	 */
+	private boolean indiceNoSuperaTamano(int indice) {
+		return indice < clientes.length;
+	}
+
+	/*
+	 * metodo Borrar Cliente (buscarIndice,
+	 * 
+	 * indiceNoSuperaTamano,desplazarUnaPosiciónHaciaIzquierda)
+	 */
+
+	/**
+	 * @param dni
+	 */
+	public void borrarCliente(String dni) {
+		int indice = buscarIndiceCliente(dni);
+		if (indiceNoSuperaTamano(indice)) {
+			desplazarUnaPosicionHaciaIzquierda(indice);
+		} else {
+			throw new ExcepcionAlquilerVehiculos("El cliente a borrar no existe");
+		}
+	}
+
+	// metodo buscarIndiceCliente (indiceNoSuperaTamano)
+	/**
+	 * @param dni
+	 * @return indice
+	 */
+
+	private int buscarIndiceCliente(String dni) {
+		int indice = 0;
+		boolean clienteEncontrado = false;
+		while (indiceNoSuperaTamano(indice) && !clienteEncontrado && clientes[indice] != null) {
+			if (clientes[indice].getDni().equals(dni))
+				clienteEncontrado = true;
+			else
+				indice++;
+		}
+		return clienteEncontrado ? indice : clientes.length;
+	}
+
+	// metodo desplazarUnaPosicionHaciaIzquierda
+	/**
+	 * @param indice
+	 */
+	private void desplazarUnaPosicionHaciaIzquierda(int indice) {
+		for (int i = indice; i < clientes.length - 1 && clientes[i] != null; i++) {
+			clientes[i] = clientes[i + 1];// desplar un indice
+		}
+		if (indice == clientes.length - 1)
+			clientes[clientes.length - 1] = null;
+	}
+
+	/* buscarCliente (indiceNoSuperaTamano */
+
+	/**
+	 * @param dni
+	 * @return cliente
+	 */
+	public Cliente buscarCliente(String dni) {
+		int posicion = buscarIndiceCliente(dni);
+		if (indiceNoSuperaTamano(posicion))
+			return new Cliente(clientes[posicion]);
+		else
+			return null;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "Clientes [clientes=" + Arrays.toString(clientes) + ", MAX_CLIENTES=" + MAX_CLIENTES + "]";
+	}
+
+}
---
+++ src/alquilerVehiculos/mvc/modelo/dao/Vehiculos.java
@@ -0,0 +1,203 @@
+package alquilerVehiculos.mvc.modelo.dao;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.Arrays;
+
+
+import alquilerVehiculos.mvc.modelo.dominio.ExcepcionAlquilerVehiculos;
+import alquilerVehiculos.mvc.modelo.dominio.vehiculo.TipoVehiculo;
+import alquilerVehiculos.mvc.modelo.dominio.vehiculo.Vehiculo;
+
+public class Vehiculos {
+
+	private Vehiculo[] vehiculos;
+	private final int MAX_VEHICULOS = 10;
+
+	
+	
+	// atributo ruta del fichero
+
+	private final String FICHERO_VEHICULOS = "datos/vehiculos.dat";
+	// constructor
+	public Vehiculos() {
+		vehiculos = new Vehiculo[MAX_VEHICULOS];
+	}
+
+	/**
+	 * @return vehiculos ( copia )
+	 */
+	public Vehiculo[] getVehiculo() {
+		return vehiculos.clone();
+	}
+
+	// metodos de lectura - escritura del fichero 
+	
+
+
+	// Metodos de lectura - escritura de fichero
+
+	public void leerVehiculos() {
+
+		// 1º crear fichero usando la constante ruta
+
+		File fichero = new File(FICHERO_VEHICULOS);
+
+		// 2º crear Objeto de la clase ObjectInputStrean
+
+		ObjectInputStream entrada;
+		try {
+			entrada = new ObjectInputStream(new FileInputStream(fichero));
+			try {
+				vehiculos = (Vehiculo[]) entrada.readObject();
+				entrada.close();
+				System.out.println("Fichero vehiculos lei­do satisfactoriamente.");
+			} catch (ClassNotFoundException e) {
+				System.out.println("No puedo encontrar la clase que tengo que leer.");
+				
+			} catch (IOException e) {
+				System.out.println("Error inesperado de Entrada/Salida.");
+			}
+		} catch (IOException e) {
+			System.out.println("No puedo abrir el fichero de vehiculos.");
+		}
+	}
+
+	public void escribirVehiculos() {
+		File fichero = new File(FICHERO_VEHICULOS);
+		try {
+			ObjectOutputStream salida = new ObjectOutputStream(new FileOutputStream(fichero));
+			salida.writeObject((Vehiculo[]) vehiculos);
+			salida.close();
+			System.out.println("Fichero vehiculos escrito satisfactoriamente");
+		} catch (FileNotFoundException e) {
+			System.out.println("No puedo crear el fichero de vehiculos");
+		} catch (IOException e) {
+			System.out.println("Error inesperado de Entrada/Salida");
+		}
+	}
+	
+
+	/*
+	 * metodo anadir Vehiculo (buscarPrimerIndiceLibre,indiceNosuperaTamaño)
+	 */
+
+	/**
+	 * @param vehiculo
+	 * @param tipoVehiculo
+	 */
+	public void anadirVehiculo(Vehiculo vehiculo, TipoVehiculo tipoVehiculo) {
+		int indice = buscarPrimerIndiceLibreComprobandoExistencia(vehiculo);
+		if (indiceNoSuperaTamano(indice))
+			/*
+			 * Modificamos se llama clase abstracta getInstancia y acceden atributos de
+			 * vehiculo
+			 */
+			vehiculos[indice] = tipoVehiculo.getInstancia(vehiculo.getMatricula(), vehiculo.getMarca(),
+					vehiculo.getModelo(), vehiculo.getDatosTecnicosVehiculo());
+		else
+			throw new ExcepcionAlquilerVehiculos("El array de  vehiculos esta lleno.");
+	}
+
+	// metodo buscarPrimerIndiceLibre (indiceNoSupera)
+	/**
+	 * @param vehiculo
+	 * @return indice
+	 */
+	private int buscarPrimerIndiceLibreComprobandoExistencia(Vehiculo vehiculo) {
+		int indice = 0;
+		boolean VehiculoEncontrado = false;
+		while (indiceNoSuperaTamano(indice) && !VehiculoEncontrado) {
+			if (vehiculos[indice] == null)
+				VehiculoEncontrado = true;
+			else if (vehiculos[indice].getMatricula().equals(vehiculo.getMatricula()))
+				throw new ExcepcionAlquilerVehiculos("Ya existe un vehiculo con esa matricula");
+			else
+				indice++;
+		}
+		return indice;
+	}
+
+	// metodo indiceNoSuperaTamano
+	/**
+	 * @param indice
+	 * @return false o true
+	 */
+	private boolean indiceNoSuperaTamano(int indice) {
+		return indice < vehiculos.length;
+	}
+
+	/* metodo de borrar Vehiculo (buscarIndiceTurismo, indiceNosuperatamaño */
+
+	/**
+	 * @param matricula
+	 */
+	public void borrarVehiculo(String matricula) {
+		int indice = buscarIndiceVehiculo(matricula);
+		if (indiceNoSuperaTamano(indice)) {
+			desplazarUnaPosicionHaciaIzquierda(indice);
+		} else {
+			throw new ExcepcionAlquilerVehiculos("El Vehiculo no existe.");
+		}
+	}
+
+	// metodo buscarIndiceVehiculo
+	/**
+	 * @param matricula
+	 * @return indice del array donde se encuente
+	 */
+	private int buscarIndiceVehiculo(String matricula) {
+		int indice = 0;
+		boolean vehiculoEncontrado = false;
+		while (indiceNoSuperaTamano(indice) && !vehiculoEncontrado) {
+
+			if (vehiculos[indice] != null && vehiculos[indice].getMatricula().equals(matricula)) {
+				vehiculoEncontrado = true;
+			} else {
+				indice++;
+			}
+		}
+		return vehiculoEncontrado ? indice : vehiculos.length;
+	}
+
+	// metodo desplazarUnaPosiciónHaciaIzquierda
+
+	/**
+	 * @param indice
+	 */
+	private void desplazarUnaPosicionHaciaIzquierda(int indice) {
+		for (int i = indice; i < vehiculos.length - 1 && vehiculos[i] != null; i++) {
+			vehiculos[i] = vehiculos[i + 1];
+		}
+	}
+
+	/* Metodo buscarTurismo ( buscarIndice,indiceNoSupera) */
+
+	/**
+	 * @param matricula
+	 * @return Vehiculo
+	 */
+	public Vehiculo buscarVehiculo(String matricula) {
+		int indice = buscarIndiceVehiculo(matricula);
+		if (indiceNoSuperaTamano(indice)) {
+			return (vehiculos[indice]);
+		} else
+			return null;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "Vehiculos [vehiculos=" + Arrays.toString(vehiculos) + ", MAX_VEHICULOS=" + MAX_VEHICULOS + "]";
+	}
+
+}
